90b577725d0f758d4601de0370f8a1abd1cda237
herberdu <0xherbertdu@gmail.com>
2024-04-10 09:14:44 +0800

    add homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ArweaveWalletKit } from 'arweave-wallet-kit';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <ArweaveWalletKit
+                    config={{
+                        permissions: ['ACCESS_ADDRESS', 'SIGN_TRANSACTION'],
+                        ensurePermissions: true,
+                    }}
+                    theme={{
+                        displayTheme: 'light',
+                    }}
+                >
+                    {children}
+                </ArweaveWalletKit>
+            </body>
+      
     </html>
   );
 }
